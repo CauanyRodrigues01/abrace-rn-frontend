efa7dd5cd87b7dadb9c0693db999134efe2d4740
Cauany Rodrigues <cauanyrodrigues01@gmail.com>
2024-10-23 20:50:21 -0300

    Criado componente botao e adicionado no header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { SobreComponent } from '../../pages/sobre/sobre.component';
+import { BotaoComponent } from '../botao/botao.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgOptimizedImage, SobreComponent],
+  imports: [NgOptimizedImage, SobreComponent, BotaoComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
