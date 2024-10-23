2590247b675883dc6ebcd3dff0c4f2f53ed12a77
Cauany Rodrigues <cauanyrodrigues01@gmail.com>
2024-10-23 17:27:13 -0300

    Adicionando o NgOptimizedImage onde falta

--- src/app/pages/doar-dinheiro/doar-dinheiro.component.ts
+++ src/app/pages/doar-dinheiro/doar-dinheiro.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-doar-dinheiro',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, NgOptimizedImage],
   templateUrl: './doar-dinheiro.component.html',
   styleUrl: './doar-dinheiro.component.scss',
 })
--- src/app/pages/galeria/galeria.component.ts
+++ src/app/pages/galeria/galeria.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-galeria',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, NgOptimizedImage],
   templateUrl: './galeria.component.html',
   styleUrl: './galeria.component.scss',
 })
